08eaf867fd6b5e8103aac8e0d75f54caf7eba71c
Isabella Hedly <304251@viauc.dk>
2024-08-09 12:54:00 +0200

    Day 1 changes

---
+++ Procore.Consoles/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var myclient = new Procore.Core.Class1();
+
+var result = await myclient.TestConnection();
+
+Console.WriteLine(result.Count);
--- Procore.Core/Class1.cs
+++ Procore.Core/Class1.cs
@@ -1,7 +1,47 @@
-﻿namespace Procore.Core
+﻿using MAD.API.Procore;
+using MAD.API.Procore.Endpoints.Companies.Models;
+using System.Net.Http.Headers;
+
+namespace Procore.Core
 {
     public class Class1
     {
+        public async Task<ArrayOfCompany> TestConnection()
+        {
+            // TODO: Get these from a configuration file or ENVIRNMENT variables.
+            var clientId = "fCgrzqNS655oEbR0etUvhct7d83Lvm7AWK4rqzpsPeo";
+            var clientSecret = "c6ENh_uzCfjXbuKLLs93fdjk6dcIpwo7nOPKfMUatMw";
 
+            var httpClient = new HttpClient();
+
+            // First get the Oauth token exchange
+
+            var exchange = new MAD.API.Procore.OAuthTokenExchange();
+
+            var token = await exchange.GetAccessToken(clientId, clientSecret, httpClient, true);
+
+            var factory = new MAD.API.Procore.DefaultProcoreApiClientFactory();
+
+            var opts = new ProcoreApiClientOptions()
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                IsSandbox = true,
+                RefreshToken = token.RefreshToken
+                
+            };
+            var clientHttpClient = factory.CreateHttpClient(opts);
+            clientHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+            var client = new MAD.API.Procore.ProcoreApiClient(clientHttpClient, opts);
+                
+                //factory.Create(opts);
+
+            var request = new MAD.API.Procore.Endpoints.Companies.ListCompaniesRequest();
+
+            var response = await client.GetResponseAsync(request);
+
+            return response.Result;
+
+        }
     }
 }
